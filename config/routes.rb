aa132089a0960fbe9f7da7e12ea0ac0a54756454
Mark van Riet <mvan.riet@hotmail.com>
2018-02-20 12:12:38 +0100

    task controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# retrieve all tasks
+get 'tasks', to: "tasks#index"
+# retrieve one task
+get 'tasks/:id', to: "tasks#show"
+# create one task
+get 'tasks/new', to: "tasks#new"
+post 'tasks', to: 'tasks#create'
+#update one task
+get 'tasks/:id/edit', to: "tasks#edit"
+patch 'tasks/id', to: "restaurants#update"
+#destroy tasks
+delete 'tasks/:id', to: "tasks#destroy"
+
 end
